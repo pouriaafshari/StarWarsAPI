0680ec91c1f6ec5eb3ff1975b6fa185678411f50
pouriaafshari <pooriyaafshari81@gmail.com>
2023-11-05 14:44:37 +0100

    cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
 const express = require('express')
 const filterRouter = require('./routes/filter')
 const searchRouter = require('./routes/search')
 const swRouter = require('./routes/sw')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use("/search", searchRouter)
 app.use("/filter", filterRouter)
